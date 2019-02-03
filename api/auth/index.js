1d5f5b32102c330e8274db3e59d24ecb076c1fb1
Timothy Santiago <santiagotimothy@gmail.com>
2019-02-04 10:41:14 +1300

    Added basic auth

--- api/auth/index.js
+++ api/auth/index.js
@@ -4,5 +4,4 @@ class Auth {
 	}
 }
 
-
 module.exports = Auth
--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require('express')
 const path = require('path')
 const auth = require('./api/auth')
+const basicAuth = require('express-basic-auth')
 
 const app = express()
 const router = express.Router()
+
+const staticAuth = basicAuth({
+	users: { admin: 'p455' },
+})
 
 router.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/containers/index/index.html'))
 })
 
-router.get('/processLogin', (req, res) => {
+router.post('/processLogin', staticAuth, (req, res) => {
 	if (auth.processLogin()) {
-		res.send('login successful')
+		res.status(200).send('success')
 	} else {
-		res.send('login failed')
+		res.status(401).send()
 	}
 })
 
 app.use('/js', express.static(path.join(__dirname, 'public/js')))
 app.use('/css', express.static(path.join(__dirname, 'public/css')))
 app.use('/img', express.static(path.join(__dirname, 'public/img')))
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
 app.use('/', router)
 module.exports = router
 
